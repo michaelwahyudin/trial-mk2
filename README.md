04e3e7502b5b622aa9020da2ad086641c416929f
michael wahyudin <michaelwahyudin90@gmail.com>
2023-11-15 20:56:40 +0700

    fix

--- README.md
+++ README.md
@@ -1,23 +1,49 @@
 # Hello i am Michael
 
-This is my [own pet project](https://trialtravel.netlify.app/) for my RevoU class
+This is my [own website](https://mypixiv.online/) website for my RevoU class
 
-_This is my pet_, visit my [Linkedin](https://www.linkedin.com/in/michael-wahyudin-922396229/?trk=people_directory&originalSubdomain=id) account ![profesional Picture][def]
+and this is my [netlify project](https://michaelpixiv.netlify.app/) website for my RevoU class
 
-[def]: ./asset/
+_This is me_, visit my [Linkedin](https://www.linkedin.com/in/michael-wahyudin-922396229/?trk=people_directory&originalSubdomain=id) account ![profesional Picture][def]
 
-**This is my progress** !
+[def]: https://media.licdn.com/dms/image/C4E03AQEqHcvQ5UQ73Q/profile-displayphoto-shrink_800_800/0/1640710136892?e=1703116800&v=beta&t=ma8T5rqPhrpxIjyQFAqs_Ak5oflwQRYD9uNsmdBCVF0
 
-- [ ] Create Semantic Html
-        1. use html tags
-        2. use headings
-        3. use p for textual content
-        4. use ol/ul
-        5. semantic 
-        6. use tag for img link etc 
-- [ ] Responsive Design
-        1. css media querries up to 375px
-        2. adaptive screen for both text and img 
-- [ ] Documentation
-- [ ] Deployment
-- [ ] http://mypixiv.online/ is online
+## Module-2 Assignment Checklist
+
+- [ ] Responsive Image
+
+  - [ ] implement contain or cover
+  - [ ] implement image srcset
+
+- [ ] Responsive asymmetrical layout
+- [ ] Implement date input
+- [ ] Font customization
+
+  - [ ] using @font-face
+  - [ ] using text-shadow
+  - [ ] using list-style
+
+- [ ] Responsive animation and transition
+
+  - [ ] using @keyframes
+  - [ ] using transition
+  - [ ] responsive
+
+- [ ] Documenting deployment
+- [ ] Connect Netlify to Github
+- [ ] Using custom domain
+
+## Deployment Process
+
+1.  create netlify account
+2.  create git repo(already done by revou) so i just pull and push my work
+3.  on netlify add new site and choose deploy with github
+4.  choose my repo (revou class 2) and click deploy
+5.  buy domain from niagahoster (i choose .online)
+6.  now i have domain from niagahoster and i need to link it into netlify
+7.  Go to my Domain page and select Add or register domain.
+8.    Enter the domain and select Verify.
+9.    confirm that I am the owner of the domain. Select Yes, add    domain to create a Netlify DNS zone for the domain.
+10.  Changed dns to dns1.p03.nsone.net -4 on niagahoster <img title="a title" alt="Alt text" src="/asset/md1.png">
+11.  DNS verification was successful <img title="a title" alt="Alt text" src="/asset/md2.png"><img title="a title" alt="Alt text" src="/asset/md3.png">
+12. http://mypixiv.online/ is online
--- script.js
+++ script.js
@@ -4,63 +4,25 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every x s
+}
+const button = document.querySelector("button");
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  alert(`This is a test!`);
 }
 
-/* scroll section */
-  document.addEventListener("DOMContentLoaded", function () {
-    const contactLink = document.querySelector('a[href="#contact"]');
-    
-    contactLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      
-      const contactSection = document.getElementById("contact");
-      
-      // Scroll to the contact section smoothly
-      contactSection.scrollIntoView({ behavior: "smooth" });
-    });
-  });
-
-    document.addEventListener("DOMContentLoaded", function () {
-      const productsLink = document.querySelector('a[href="#products"]');
-      
-      productsLink.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-        const productsSection = document.getElementById("products");
-        
-        // Scroll to the products section smoothly
-        productsSection.scrollIntoView({ behavior: "smooth" });
-      });
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-      const aboutUsLink = document.querySelector('a[href="#aboutUs"]');
-      
-      aboutUsLink.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-        const aboutUsSection = document.getElementById("aboutUs");
-        
-        // Scroll to the "About Us" section smoothly
-        aboutUsSection.scrollIntoView({ behavior: "smooth" });
-      });
-    }); 
-  
-
 /* burger section */
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenu = document.getElementById("hamburger-menu");
 const hamburgerContent = document.querySelector(".hamburger-content");
 
 let isOpen = false; // Variable to track the state of the hamburger menu
@@ -72,5 +34,28 @@ hamburgerMenu.addEventListener("click", () => {
     hamburgerContent.classList.add("show"); // Add the "show" class to display the content
   } else {
     hamburgerContent.classList.remove("show"); // Remove the "show" class to hide the content
+  }
+});
+
+// Add event listener to update the displayed value
+const rangeSlider = document.getElementById("myRange");
+const rangeValue = document.querySelector(".range-value");
+
+rangeSlider.addEventListener("input", function () {
+  rangeValue.textContent = rangeSlider.value;
+});
+
+// Get the night mode switch element
+const nightModeSwitch = document.getElementById("night-mode-switch");
+
+// Add an event listener to listen for changes in the switch state
+nightModeSwitch.addEventListener("change", () => {
+  const body = document.body;
+  if (nightModeSwitch.checked) {
+    // If the switch is checked, enable night mode (e.g., change background color)
+    body.classList.add("night-mode");
+  } else {
+    // If the switch is unchecked, disable night mode
+    body.classList.remove("night-mode");
   }
 });
